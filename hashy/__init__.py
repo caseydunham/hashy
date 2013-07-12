e27726695cb24e071a0036d865dad0ce33778d38
Casey Dunham <casey.dunham@gmail.com>
2013-07-11 21:06:26 -0400

    updating to version 0.4.0

--- hashy/__init__.py
+++ hashy/__init__.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-__version__ = '0.3'
+__version__ = '0.4.0'
 __author__ = "Casey Dunham"
 __license__ = 'MIT'
 
